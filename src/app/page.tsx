3204a698a1b24ee0caefc21a99e7628f86771583
OferSarid-Reco <ofers@reco.ai>
2024-05-20 22:55:05 +0300

    wip

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import styled from 'styled-components';
+
+export default function Home() {
+  return <AppContainer></AppContainer>;
+}
+
+const AppContainer = styled.div`
+  background-color: #ff0000;
+  color: #ffffff;
+`;
