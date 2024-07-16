6d244839750548bcf2a8aed987024b575eb0ade4
Ofer Sarid <ofer@delta.band>
2024-07-17 00:08:59 +0300

    wip

--- src/main-layout/MainLayout.stories.tsx
+++ src/main-layout/MainLayout.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 // import { fn } from '@storybook/test';
-import { MainLayout } from '@/main-layout';
+import { MainLayout } from '.';
 
 const meta = {
   title: 'App/MainLayout',
